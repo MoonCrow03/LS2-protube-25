b2c314f481a2f9226c47903c6763c45b17f9bd96
Júlia Ting Castro Sevilla <juliating03@hotmail.com>
2024-10-02 13:44:49 +0200

    VideoService

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/controller/VideoController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/controller/VideoController.java
@@ -1,5 +1,7 @@
 package com.tecnocampus.LS2.protube_back.controller;
 
+import com.tecnocampus.LS2.protube_back.services.VideoServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/videos")
 public class VideoController {
 
+    @Autowired
+    private VideoServices videoServices;
+
     @GetMapping("/view")
     public ResponseEntity<String> getVideo() {
-        return ResponseEntity.ok().body("Hello World");
+        return ResponseEntity.ok().body(videoServices.getVideos().toString());
     }
 
-    
+
 
 
 
---
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/services/VideoServices.java
@@ -0,0 +1,12 @@
+package com.tecnocampus.LS2.protube_back.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VideoServices {
+    public List<String> getVideos() {
+        return List.of("video1", "video2", "video3");
+    }
+}
