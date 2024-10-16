5672591894061f08a52b604604118c6a5f8b2190
Cesc <cesc38e@gmail.com>
2024-10-16 14:01:08 +0200

    oauth log out

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/controller/IndexController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/controller/IndexController.java
@@ -1,15 +1,22 @@
 package com.tecnocampus.LS2.protube_back.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("")
-    public ModelAndView home() {
-        return new ModelAndView("index");
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        if (principal != null) {
+            model.addAttribute("profile", principal.getClaims());
+        }
+        return "index";
     }
 
 }
--- backend/src/main/java/com/tecnocampus/LS2/protube_back/security/SecurityConfig.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/security/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.tecnocampus.LS2.protube_back.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,13 +17,34 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+    @Value("${okta.oauth2.client-id}")
+    private String clientId;
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(withDefaults());
+                .oauth2Login(withDefaults())  // OAuth2 Login
+                .logout(logout -> logout
+                        .addLogoutHandler(logoutHandler()));
         return http.build();
+
+    }
+
+    private LogoutHandler logoutHandler() {
+        return (request, response, authentication) -> {
+            try {
+                //String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                String baseUrl = "localhost:8080";
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
