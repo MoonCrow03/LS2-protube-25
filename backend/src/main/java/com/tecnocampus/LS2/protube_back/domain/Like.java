f2f1eafd13133e66a07048a97eac319e5d7f9288
RubenMailloBaena <rubenmaillo2003@gmail.com>
2024-10-16 15:50:11 +0200

    Update Like.java

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/Like.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/Like.java
@@ -3,11 +3,13 @@ package com.tecnocampus.LS2.protube_back.domain;
 import com.tecnocampus.LS2.protube_back.domain.entityId.LikeId;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "pt_likes")
 @NoArgsConstructor
 @Getter
 @Setter
