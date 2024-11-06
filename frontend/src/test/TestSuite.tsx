2f24896fe98aa8bde66e611271a5812c01a74560
Júlia Ting Castro Sevilla <juliating03@hotmail.com>
2024-11-06 12:47:48 +0100

    Create video testing + fix

---
+++ frontend/src/test/TestSuite.tsx
@@ -0,0 +1,19 @@
+describe('TestSuite', () => {
+    beforeAll(() => {
+
+    });
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+
+    it('should pass', () => {
+        expect(true).toBe(true)
+    });
+
+    test('value is set to true when button is clicked', () => {
+        expect(true).toBeTruthy();
+    })
+})
