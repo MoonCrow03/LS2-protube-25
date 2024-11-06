d4f8255480e6ec502c50593e037bb2cbe81b7ee5
RubenMailloBaena <rubenmaillo2003@gmail.com>
2024-11-06 13:56:03 +0100

    validations

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/api/CommentRestController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/api/CommentRestController.java
@@ -3,6 +3,7 @@ package com.tecnocampus.LS2.protube_back.api;
 import com.tecnocampus.LS2.protube_back.dto.CommentDTO;
 import com.tecnocampus.LS2.protube_back.dto.record.InputCommentRecord;
 import com.tecnocampus.LS2.protube_back.services.CommentService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class CommentRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CommentDTO createComment(@RequestBody InputCommentRecord inputComment) {
+    public CommentDTO createComment(@Valid @RequestBody InputCommentRecord inputComment) {
         return commentService.createComment(inputComment);
     }
 
