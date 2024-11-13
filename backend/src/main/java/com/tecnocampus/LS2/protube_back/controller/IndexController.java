b7553e27ef421c0fefe0a474d23ee215c0cf3b2e
Cesc <cesc38e@gmail.com>
2024-11-13 12:45:28 +0100

    login test

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/controller/IndexController.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/controller/IndexController.java
@@ -27,7 +27,9 @@ public class IndexController {
             String picture = principal.getPicture();
 
             userService.createUser(new InputUserRecord(name,email,picture,auth0Id));
+            return "redirect:http://localhost:5173";
         }
+
         return "index";
     }
 
